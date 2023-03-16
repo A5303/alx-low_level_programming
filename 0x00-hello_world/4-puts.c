b0a4963c9f82f59fb77a54389ef4a767b03cef66
Magdaline <Keeshawamboh@gmail.com>
2023-03-16 14:25:27 -0700

    C is a programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using puts function
- *
- * Return: Always 0 (Success)
- */
+ *  * main - A program that prints a line with puts function
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-}	
+			return (0);
+}
