9f9bb95bc8cea0e3c72e39076717477b5911087d
Magdaline <Keeshawamboh@gmail.com>
2023-03-23 13:53:10 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 !=)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
