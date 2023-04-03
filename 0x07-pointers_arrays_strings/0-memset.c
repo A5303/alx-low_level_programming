92007e85556001fb41155ba67f4781dcc249cc47
Magdaline <Keeshawamboh@gmail.com>
2023-04-03 11:58:17 -0700

    function that copies memory area

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
+ * @g: starting address of memory to be filled
+ * @h: the desired value
+ * @y: number of bytes to be changed
  *
- * Return: changed array with new value for n bytes
+ * Return: changed array with new value for y bytes
  */
 char *_memset(char *g, char h, unsigned int y)
 {
